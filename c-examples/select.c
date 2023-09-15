6c846c2548bf5e7f4dd46679b9bff62a8189b454
cdufour <opusidea@gmail.com>
2023-09-15 18:28:37 +0200

    end

--- c-examples/select.c
+++ c-examples/select.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
-#include <string.h>
 
 static int callback(void *data, int argc, char **argv, char **azColName)
 {
     int i;
-    fprintf(stdout, "%s: ", (const char *)data);
+    fprintf(stderr, "%s: ", (const char *)data);
 
     for (i = 0; i < argc; i++)
     {
-        printf("%s : %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
-    
+
     printf("\n");
     return 0;
 }
@@ -22,9 +21,8 @@ int main(int argc, char *argv[])
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
-    char *sql1;
-    char sql2[10];
-    const char *data = "Callback function for: ";
+    char *sql;
+    const char *data = "Callback function called";
 
     /* Open database */
     rc = sqlite3_open("test.db", &db);
@@ -40,16 +38,9 @@ int main(int argc, char *argv[])
     }
 
     /* Create SQL statement */
-    sql1 = "SELECT AGE, SALARY from COMPANY where ID = ";
-    strcpy(sql2, argv[1]);
-    //printf("%s\n", sql2);
-
-    char * sql = (char *) malloc( sizeof(sql1) + sizeof(sql2) );
-    strcpy(sql, sql1);
-    strcat(sql, sql2);
+    sql = "SELECT * from COMPANY";
 
     /* Execute SQL statement */
-    
     rc = sqlite3_exec(db, sql, callback, (void *)data, &zErrMsg);
 
     if (rc != SQLITE_OK)
@@ -61,7 +52,6 @@ int main(int argc, char *argv[])
     {
         fprintf(stdout, "Operation done successfully\n");
     }
-    
     sqlite3_close(db);
     return 0;
 }
